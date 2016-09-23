6d35b818ce0af1973b58faa94a3504cc35b00e88
Vic Zhu <vicxizhu@gmail.com>
2016-09-22 23:34:31 -0400

    add components dir for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 import { ApiService } from './shared';
 import { routing } from './app.routing';
 
